980ecd8b8cea5d8872cdc6dcab811711202307eb
ShinHyungJune <ssa4141@naver.com>
2025-01-09 10:13:46 +0900

    농가 이미지 추가

--- app/Models/FarmStory.php
+++ app/Models/FarmStory.php
@@ -6,12 +6,34 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use PhpParser\Node\Attribute;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class FarmStory extends Model
+class FarmStory extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $guarded = ['id'];
+
+    public function registerMediaCollections():void
+    {
+        $this->addMediaCollection('img')->singleFile();
+    }
+
+    public function getImgAttribute()
+    {
+        if($this->hasMedia('img')) {
+            $media = $this->getMedia('img')[0];
+
+            return [
+                "id" => $media->id,
+                "name" => $media->file_name,
+                "url" => $media->getFullUrl()
+            ];
+        }
+
+        return null;
+    }
 
     public function user(): BelongsTo
     {
