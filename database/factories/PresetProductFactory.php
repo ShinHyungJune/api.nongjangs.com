db08ead44ae7d411607022a9c9d3e073cbcce7d1
ShinHyungJune <ssa4141@naver.com>
2025-01-31 13:34:29 +0900

    fix

--- database/factories/PresetProductFactory.php
+++ database/factories/PresetProductFactory.php
@@ -36,6 +36,8 @@ class PresetProductFactory extends Factory
             'option_price' => $option->price,
             'option_type' => $option->type,
 
+            'package_setting_active' => rand(0,1),
+            'package_type' => $package->type,
             'package_name' => "야채꾸러미",
             'package_count' => $package->count,
             'package_price' => $package->price_single,
