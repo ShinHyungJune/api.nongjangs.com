caa4a36e94098178ab350fbeaa8dc9e8540d39a6
ShinHyungJune <ssa4141@naver.com>
2025-02-04 17:55:49 +0900

    fix

--- app/Http/Resources/MaterialResource.php
+++ app/Http/Resources/MaterialResource.php
@@ -28,7 +28,7 @@ class MaterialResource extends JsonResource
             'price' => $this->pivot ? $this->pivot->price : '',
             'price_origin' => $this->pivot ? $this->pivot->price_origin : '',
             'value' => $this->pivot ? $this->pivot->value : '',
-            'ratio_discount' => $this->pivot ? floor((($this->pivot->price_origin - $this->pivot->price) / $this->pivot->price_origin) * 100) : '',
+            'ratio_discount' => $this->pivot && $this->pivot->price_origin > 0 ? floor((($this->pivot->price_origin - $this->pivot->price) / $this->pivot->price_origin) * 100) : 0,
             'unit' => $this->pivot ? $this->pivot->unit : '',
             'count' => $this->pivot ? $this->pivot->count : '',
         ];
