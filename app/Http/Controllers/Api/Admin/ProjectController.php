767a8b3136a84ccb21bf428d56fced3c2332e4eb
ShinHyungJune <ShinHyungJune>
2025-02-26 08:33:00 +0900

    농가살리기 추가

---
+++ app/Http/Controllers/Api/Admin/ProjectController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProjectResource;
+use App\Http\Requests\ProjectRequest;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProjectController extends ApiController
+{
+    /** 목록
+     * @group 관리자
+     * @subgroup Project(project)
+     * @responseFile storage/responses/projects.json
+     */
+    public function index(ProjectRequest $request)
+    {
+        $items = new Project();
+
+        if($request->word){
+            $items = $items->where(function (Builder $query) use($request) {
+                $query->whereHas('product', function ($query) use($request){
+                    $query->where('title', 'LIKE', '%'.$request->word.'%')
+                        ->orWhere('id', 'LIKE', '%'.$request->word.'%')
+                        ->orWhereHas('farm', function ($query) use ($request){
+                            $query->where('title', 'LIKE', '%'.$request->word.'%');
+                        });
+                });
+            });
+        }
+
+        $items = $items->latest()->paginate(25);
+
+        return ProjectResource::collection($items);
+    }
+
+    /** 상세
+     * @group 관리자
+     * @subgroup Project(project)
+     * @responseFile storage/responses/project.json
+     */
+    public function show(Project $project)
+    {
+        return $this->respondSuccessfully(ProjectResource::make($project));
+    }
+
+    /** 생성
+     * @group 관리자
+     * @subgroup Project(project)
+     * @responseFile storage/responses/project.json
+     */
+    public function store(ProjectRequest $request)
+    {
+        $createdItem = Project::create($request->validated());
+
+        $createdItem->tags()->sync($request->tag_ids);
+
+        if(is_array($request->file("files"))){
+            foreach($request->file("files") as $file){
+                $createdItem->addMedia($file["file"])->toMediaCollection("img", "s3");
+            }
+        }
+
+        return $this->respondSuccessfully(ProjectResource::make($createdItem));
+    }
+
+    /** 수정
+     * @group 관리자
+     * @subgroup Project(project)
+     * @responseFile storage/responses/project.json
+     */
+    public function update(ProjectRequest $request, Project $project)
+    {
+        $project->update($request->validated());
+
+        $project->tags()->sync($request->tag_ids);
+
+        if($request->files_remove_ids){
+            $medias = $project->getMedia("img");
+
+            foreach($medias as $media){
+                foreach($request->files_remove_ids as $id){
+                    if((int) $media->id == (int) $id){
+                        $media->delete();
+                    }
+                }
+            }
+        }
+
+        if(is_array($request->file("files"))){
+            foreach($request->file("files") as $file){
+                $project->addMedia($file["file"])->toMediaCollection("img", "s3");
+            }
+        }
+
+        return $this->respondSuccessfully(ProjectResource::make($project));
+    }
+
+    /** 삭제
+     * @group 관리자
+     * @subgroup Project(project)
+     */
+    public function destroy(ProjectRequest $request)
+    {
+        Project::whereIn('id', $request->ids)->delete();
+
+        return $this->respondSuccessfully();
+    }
+}
--- app/Http/Requests/ProjectRequest.php
+++ app/Http/Requests/ProjectRequest.php
@@ -20,20 +20,24 @@ class ProjectRequest extends FormRequest
 
                 case 'store':
                     return [
-                        'product_id' => ['required', 'exists:products'],
+                        'product_id' => ['required', 'integer'],
+                        'files' => ['required', 'array'],
                         'started_at' => ['required', 'date'],
                         'finished_at' => ['required', 'date'],
                         'count_goal' => ['required', 'integer'],
                         'count_participate' => ['required', 'integer'],
+                        'tag_ids' => ['nullable', 'array'],
                     ];
 
                 case 'update':
                     return [
-                        'product_id' => ['required', 'exists:products'],
+                        'product_id' => ['required', 'integer'],
+                        'files' => ['required', 'array'],
                         'started_at' => ['required', 'date'],
                         'finished_at' => ['required', 'date'],
                         'count_goal' => ['required', 'integer'],
-                        'count_participate' => ['required', 'integer'],//
+                        'count_participate' => ['required', 'integer'],
+                        'tag_ids' => ['nullable', 'array'],
                     ];
 
                 case 'destroy':
@@ -72,8 +76,23 @@ class ProjectRequest extends FormRequest
     {
         return [
             // 이 모델만 쓰이는 애들
-            'example' => [
-                'description' => '<span class="point"></span>',
+            'product_id' => [
+                'description' => '<span class="point">상품 고유번호</span>',
+            ],
+            'started_at' => [
+                'description' => '<span class="point">시작일자</span>',
+            ],
+            'finished_at' => [
+                'description' => '<span class="point">종료일자</span>',
+            ],
+            'count_goal' => [
+                'description' => '<span class="point">목표인원</span>',
+            ],
+            'count_participate' => [
+                'description' => '<span class="point">참여인원</span>',
+            ],
+            'tag_ids' => [
+                'description' => '<span class="point">태그고유번호목록</span>',
             ],
 
             // 늘 쓰이는 애들
