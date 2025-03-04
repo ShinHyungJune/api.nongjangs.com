d2f5a13b03d1a30630138a334ec5cf8ae3d39e3f
ShinHyungJune <ssa4141@naver.com>
2025-03-04 16:46:56 +0900

    농가이야기 사용자정보 등록

--- app/Http/Controllers/Api/Admin/FarmStoryController.php
+++ app/Http/Controllers/Api/Admin/FarmStoryController.php
@@ -50,7 +50,9 @@ class FarmStoryController extends ApiController
     {
         $request['tags'] = $request->tags ?? [];
 
-        $createdItem = FarmStory::create($request->validated());
+        $createdItem = FarmStory::create(array_merge($request->validated(), [
+            'user_id' => auth()->id()
+        ]));
 
         $createdItem->tags()->sync(array_column($request->tags, 'id'));
 
